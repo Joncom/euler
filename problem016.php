69e12acc8ede4c9281aafd31b0a9f385565e51d0
Jonathan Commins <joncom@gmail.com>
2012-11-01 23:01:10 -0700

    Find sum of digits in string.

--- problem016.php
+++ problem016.php
@@ -36,6 +36,10 @@ echo "<pre>";
 // String value of digits for the number 2^1000.
 $digits = gmp_strval( gmp_pow( 2, 1000 ) );
 
+$sum = 0;
+
+for( $i = 0; $i < strlen( $digits ); $i++  ) $sum += $digits[ $i ];
+
 echo "</pre>";
 
 ?>
