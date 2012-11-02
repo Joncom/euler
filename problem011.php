63c33789f9f36f5b5faffba311705a828f843a0c
Jonathan Commins <joncom@gmail.com>
2012-11-02 00:05:16 -0700

    Created productUpRight().

--- problem011.php
+++ problem011.php
@@ -133,6 +133,21 @@ function productUp( $grid, $index, $width ) {
 
 }
 
+function productUpRight( $grid, $index, $width ) {
+
+    $height = count( $grid ) / $width;
+
+    // Return -1 if series is invalid.
+    if( ( floor( $index / $height ) ) - 3 < 0 ) return -1;
+    if( ( $index % $width ) + 4 > $width ) return -1;
+
+    return $grid[ $index ]
+         * $grid[ $index - ( 1 * $width ) + 1 ]
+         * $grid[ $index - ( 2 * $width ) + 2 ]
+         * $grid[ $index - ( 3 * $width ) + 3 ];
+
+}
+
 function productDown( $grid, $index, $width ) {
 
     $height = count( $grid ) / $width;
