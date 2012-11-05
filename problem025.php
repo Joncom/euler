d09b1d1b778561e5a9138e5f04afe8b41c5c3a30
Jonathan Commins <joncom@gmail.com>
2012-11-05 13:50:24 -0800

    Started problem 25.

---
+++ problem025.php
@@ -0,0 +1,65 @@
+<?php
+function getTime()
+    {
+    $a = explode (' ',microtime());
+    return(double) $a[0] + $a[1];
+    }
+$Start = getTime();
+?>
+
+
+
+
+
+<?php
+
+/**************************
+/
+/ The Fibonacci sequence is defined by the recurrence relation:
+/
+/ F_n = F_(n-1) + F_(n-2), where F_1 = 1 and F_2 = 1.
+/ Hence the first 12 terms will be:
+/
+/ F_1 = 1
+/ F_2 = 1
+/ F_3 = 2
+/ F_4 = 3
+/ F_5 = 5
+/ F_6 = 8
+/ F_7 = 13
+/ F_8 = 21
+/ F_9 = 34
+/ F_10 = 55
+/ F_11 = 89
+/ F_12 = 144
+/ The 12th term, F_12, is the first term to contain three digits.
+/
+/ What is the first term in the Fibonacci sequence to contain 1000 digits?
+/
+/ Solution:
+/ Running-time:
+/
+/
+**************************/
+
+set_time_limit( 30 );
+
+// Do work here.
+
+echo "<pre>";
+
+echo "Answer: ";
+
+echo "</pre>";
+
+?>
+
+
+
+
+
+
+<?php
+$End = getTime();
+echo "Time taken = ".number_format(($End - $Start),2)." secs";
+?>
