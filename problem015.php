332cd94253c83e23d35f94e947b7b477241cb7b4
Jonathan Commins <joncom@gmail.com>
2012-11-05 13:10:11 -0800

    First attempt at a recursive solution.

--- problem015.php
+++ problem015.php
@@ -19,14 +19,51 @@ $Start = getTime();
 / there are 6 routes (without backtracking) to
 / the bottom right corner.
 /
-/ How many routes are there through a 2020 grid?
+/ How many routes are there through a 20x20 grid?
 /
 / Solution: ?
 / Running-time: ?
 /
 **************************/
 
-set_time_limit(30);
+set_time_limit(300);
+
+function countRoutes( $x, $y, $grid_width, $grid_height ) {
+
+    // Reached the end?
+    if( $x == $grid_width && $y == $grid_height ) {
+
+        return 1;
+
+    }
+
+    $local_count = 0;
+
+    // Try Right.
+    if( $x < $grid_width ) {
+
+        $local_count += countRoutes( $x + 1, $y, $grid_width, $grid_height );
+
+    }
+
+    // Try Down.
+    if( $y < $grid_height ) {
+
+        $local_count += countRoutes( $x, $y + 1, $grid_width, $grid_height );
+
+    }
+
+    return $local_count;
+
+}
+
+$grid_width = 20;
+$grid_height = 20;
+
+// Possible route count?
+$answer = countRoutes( 0, 0, $grid_width, $grid_height );
+
+echo $answer . " routes.";
 
 
 ?>
