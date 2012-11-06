10ba0f6ab5e6e1a1276bd70ca56ff24b5046f339
Jonathan Commins <joncom@gmail.com>
2012-11-06 15:20:33 -0800

    Added solution and time elapsed.

--- problem024.php
+++ problem024.php
@@ -27,8 +27,8 @@ $Start = getTime();
 / What is the millionth lexicographic permutation of
 / the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 /
-/ Solution: ?
-/ Running-time: ?
+/ Solution: 2783915460
+/ Running-time: 22.96 secs
 /
 /
 **************************/
