603fcf7956fac70077f542d6301c4b720cde5826
Jonathan Commins <joncom@gmail.com>
2012-11-05 20:49:54 -0800

    Created table to getting a numeric value from a letter.

--- problem022.php
+++ problem022.php
@@ -36,6 +36,37 @@ $Start = getTime();
 
 set_time_limit( 30 );
 
+$letter_to_number = array(
+
+	'A' => 1,
+	'B' => 2,
+	'C' => 3,
+	'D' => 4,
+	'E' => 5,
+	'F' => 6,
+	'G' => 7,
+	'H' => 8,
+	'I' => 9,
+	'J' => 10,
+	'K' => 11,
+	'L' => 12,
+	'M' => 13,
+	'N' => 14,
+	'O' => 15,
+	'P' => 16,
+	'Q' => 17,
+	'R' => 18,
+	'S' => 19,
+	'T' => 20,
+	'U' => 21,
+	'V' => 22,
+	'W' => 23,
+	'X' => 24,
+	'Y' => 25,
+	'Z' => 26
+
+);
+
 // Load names from external file.
 $names = file_get_contents( 'problem022_names.txt' );
 
